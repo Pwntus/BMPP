c8e9ba9682752625d4b6114e91a55831650658e9
Pwntus <pontus.edvard@gmail.com>
2016-09-09 23:25:08 +0200

    Optimized parser

--- bmpplib.c
+++ bmpplib.c
@@ -9,52 +9,28 @@ void error(char *err_msg) {
 	exit(0);
 }
 
-FILE * bmp_open(char *bmp_fn) {
-
-	FILE *bmp_f = fopen(bmp_fn, "r");
-	if (bmp_f == NULL)
-		error("Failed to open file");
-
-	return bmp_f;
-}
-
-void bmp_read(void *data, FILE *file, size_t size) {
-	fread(data, size, 1, file);
-}
-
-void read_file_header(file_header_t *data, FILE *file) {
-	fseek(file, BMP_FILE_HEADER_OFFSET, SEEK_SET);
-
-	bmp_read(&data->signature, file, sizeof(data->signature));
-	bmp_read(&data->filesize, file, sizeof(data->filesize));
-	bmp_read(&data->reserved_0, file, sizeof(data->reserved_0));
-	bmp_read(&data->reserved_1, file, sizeof(data->reserved_1));
-	bmp_read(&data->offset, file, sizeof(data->offset));
-}
-
 uint8_t bmp_parse(char *bmp_fn) {
 	FILE *bmp_f;
+	header_t header;
 
-	file_header_t file_header;
-
-	if (bmp_fn == NULL)
-		error("Bitmap file name");
-
-	bmp_f = bmp_open(bmp_fn);
+	if ((bmp_f = fopen(bmp_fn, "r")) == NULL)
+		error("Failed to open file");
 	
 	/* BITMAP FILE HEADER
-	*
-	 * Size: 	14 bytes
-	 * Content:	general information about
-	 *			the bitmap image file.
+	 *
+	 * Size     14 bytes
+	 * Content: general information about
+	 *          the bitmap image file.
 	 */
-	read_file_header(&file_header, bmp_f);
+	fseek(bmp_f, BMP_FILE_HEADER_OFFSET, SEEK_SET);
+	if (fread(&header, sizeof(header), 1, bmp_f) != 1)
+		error("Failed to read file header");
 
-	printf("Signature: 0x%X\n", (uint16_t)file_header.signature);
-	printf("Filesize: 0x%X (%d KB)\n", (uint32_t)file_header.filesize, (uint32_t)(file_header.filesize / 1000));
-	printf("R0: 0x%X\n", (uint16_t)file_header.reserved_0);
-	printf("R1: 0x%X\n", (uint16_t)file_header.reserved_1);
-	printf("Offset: 0x%X\n", (uint32_t)file_header.offset);
+	printf("Signature: 0x%X\n", (uint16_t)header.signature);
+	printf("Filesize: 0x%X (%d KB)\n", (uint32_t)header.filesize, (uint32_t)(header.filesize / 1000));
+	printf("R0: 0x%X\n", (uint16_t)header.reserved_0);
+	printf("R1: 0x%X\n", (uint16_t)header.reserved_1);
+	printf("Offset: 0x%X\n", (uint32_t)header.offset);
 
 	fclose(bmp_f);
 
--- bmpplib.h
+++ bmpplib.h
@@ -7,19 +7,15 @@ enum {
 	BMP_FILE_HEADER_OFFSET = 0
 };
 
-typedef struct file_header_t {
+typedef struct header_t {
 	uint16_t 	signature;
 	uint32_t 	filesize;
 	uint16_t	reserved_0;
 	uint16_t	reserved_1;
 	uint32_t	offset;
-} file_header_t;
+} __attribute__((packed)) header_t;
 
 void error(char *error_msg);
-FILE * bmp_open(char *bmp_fn);
-void bmp_read(void *data, FILE *file, size_t size);
 uint8_t bmp_parse(char *bmp_fn);
-
-void read_file_header(file_header_t *data, FILE *file);
 
 #endif /* !BMPPLIB_H */
